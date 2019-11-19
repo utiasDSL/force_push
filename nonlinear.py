6a0b9e2d08b0f5381f279de1a405ce42951dd14d
Adam Heins <mail@adamheins.com>
2019-11-19 18:22:16 -0500

    Clean up nonlinear sim.

--- nonlinear.py
+++ nonlinear.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 import numpy as np
-import numpy.matlib as matlib
 import matplotlib.pyplot as plt
 import qpoases
 
@@ -23,7 +22,7 @@ class Pendulum(object):
         # TODO do I need to put modulo math in here?
         return x + self.dt * np.array([
             x[1],
-            np.sin(x[0]) + u])  # TODO
+            self.g * np.sin(x[0]) / self.l + u])
 
     def measure(self, x):
         ''' Measurement model: y_k = g(u_k) '''
@@ -42,79 +41,6 @@ class Pendulum(object):
         return np.eye(self.n)
 
 
-def lookahead(sys, x0, Yd, U, Q, R, N):
-    ''' Generate lifted matrices proprogating the state N timesteps into the
-        future.
-
-        sys: System model.
-        x0:  Current state.
-        Yd:  Desired output trajectory.
-        U:   Input trajectory from the last iteration.
-        Q:   Tracking error weighting matrix.
-        R:   Input magnitude weighting matrix.
-        N:   Number of timesteps into the future. '''
-
-    n = sys.n
-    m = sys.m
-    p = sys.p
-
-    # States from the last iteration
-    X_last = np.zeros(n*(N+1))
-    X_last[:n] = x0
-
-    for k in range(1, N+1):
-        x_prev = X_last[(k-1)*n:k*n]
-        u_prev = U[(k-1)*m:k*m]
-        x = sys.motion(x_prev, u_prev)
-        X_last[k*n:(k+1)*n] = x
-
-    # Arrays of the linearized matrices for each timestep.
-    As = np.zeros((n*(N+1), n))
-    Bs = np.zeros((n*N, m))
-    Cs = np.zeros((p*(N+1), n))
-
-    # Calculate B matrices: B_0..B_{N-1}
-    for k in range(N):
-        x = X_last[k*n:(k+1)*n]
-        Bs[k*n:(k+1)*n, :] = sys.calc_B(x)
-
-    # Calculate A and C matrices: A_1..A_N, C_1..C_N
-    for k in range(1, N+1):
-        x = X_last[k*n:(k+1)*n]
-        As[k*n:(k+1)*n, :] = sys.calc_A(x)
-        Cs[k*p:(k+1)*p, :] = sys.calc_C(x)
-
-    Abar = np.zeros(p*N)
-    Bbar = np.zeros((p*N, m*N))
-    Qbar = np.zeros((p*N, p*N))
-    Rbar = np.zeros((m*N, m*N))
-
-    # Build Abar matrix
-    for k in range(N):
-        x = X_last[(k+1)*n:(k+2)*n]
-        Abar[k*p:(k+1)*p] = sys.measure(x)
-
-    # Build Bbar matrix
-    for r in range(N):
-        for c in range(r+1):
-            B = Bs[c*n:(c+1)*n, :]
-            C = Cs[(r+1)*p:(r+2)*p, :]
-            A = np.eye(n)
-            for i in range(c+1, r+1):
-                A = As[i*n:(i+1)*n, :].dot(A)
-            Bbar[r*p:(r+1)*p, c*m:(c+1)*m] = C.dot(A).dot(B)
-
-    # Build lifted weight matrices
-    for k in range(N):
-        Qbar[k*p:(k+1)*p, k*p:(k+1)*p] = Q
-        Rbar[k*m:(k+1)*m, k*m:(k+1)*m] = R
-
-    H = Rbar + Bbar.T.dot(Qbar).dot(Bbar)
-    g = U.T.dot(Rbar) + (Abar - Yd).T.dot(Qbar).dot(Bbar)
-
-    return H, g
-
-
 class MPC(object):
     ''' Model predictive controller. '''
     def __init__(self, sys, Q, R, lb, ub):
@@ -125,7 +51,76 @@ class MPC(object):
         self.ub = ub
 
     def _lookahead(self, x0, Yd, U, N):
-        return lookahead(self.sys, x0, Yd, U, self.Q, self.R, N)
+        ''' Generate lifted matrices proprogating the state N timesteps into the
+            future.
+
+            sys: System model.
+            x0:  Current state.
+            Yd:  Desired output trajectory.
+            U:   Input trajectory from the last iteration.
+            Q:   Tracking error weighting matrix.
+            R:   Input magnitude weighting matrix.
+            N:   Number of timesteps into the future. '''
+
+        n = self.sys.n
+        m = self.sys.m
+        p = self.sys.p
+
+        # States from the last iteration
+        X_last = np.zeros(n*(N+1))
+        X_last[:n] = x0
+
+        for k in range(1, N+1):
+            x_prev = X_last[(k-1)*n:k*n]
+            u_prev = U[(k-1)*m:k*m]
+            x = self.sys.motion(x_prev, u_prev)
+            X_last[k*n:(k+1)*n] = x
+
+        # Arrays of the linearized matrices for each timestep.
+        As = np.zeros((n*(N+1), n))
+        Bs = np.zeros((n*N, m))
+        Cs = np.zeros((p*(N+1), n))
+
+        # Calculate B matrices: B_0..B_{N-1}
+        for k in range(N):
+            x = X_last[k*n:(k+1)*n]
+            Bs[k*n:(k+1)*n, :] = self.sys.calc_B(x)
+
+        # Calculate A and C matrices: A_1..A_N, C_1..C_N
+        for k in range(1, N+1):
+            x = X_last[k*n:(k+1)*n]
+            As[k*n:(k+1)*n, :] = self.sys.calc_A(x)
+            Cs[k*p:(k+1)*p, :] = self.sys.calc_C(x)
+
+        Abar = np.zeros(p*N)
+        Bbar = np.zeros((p*N, m*N))
+        Qbar = np.zeros((p*N, p*N))
+        Rbar = np.zeros((m*N, m*N))
+
+        # Build Abar matrix
+        for k in range(N):
+            x = X_last[(k+1)*n:(k+2)*n]
+            Abar[k*p:(k+1)*p] = self.sys.measure(x)
+
+        # Build Bbar matrix
+        for r in range(N):
+            for c in range(r+1):
+                B = Bs[c*n:(c+1)*n, :]
+                C = Cs[(r+1)*p:(r+2)*p, :]
+                A = np.eye(n)
+                for i in range(c+1, r+1):
+                    A = As[i*n:(i+1)*n, :].dot(A)
+                Bbar[r*p:(r+1)*p, c*m:(c+1)*m] = C.dot(A).dot(B)
+
+        # Build lifted weight matrices
+        for k in range(N):
+            Qbar[k*p:(k+1)*p, k*p:(k+1)*p] = self.Q
+            Rbar[k*m:(k+1)*m, k*m:(k+1)*m] = self.R
+
+        H = Rbar + Bbar.T.dot(Qbar).dot(Bbar)
+        g = U.T.dot(Rbar) + (Abar - Yd).T.dot(Qbar).dot(Bbar)
+
+        return H, g
 
     def _iterate(self, x0, Yd, U, N):
         # Create the QP, which we'll solve sequentially.
@@ -173,14 +168,6 @@ class MPC(object):
 
         # return first optimal input
         return U[:self.sys.m]
-
-
-def step(t):
-    ''' Desired trajectory '''
-    # step
-    if t < 1.0:
-        return 0.0
-    return 1.0
 
 
 def main():
@@ -244,9 +231,10 @@ def main():
         xs[i+1, :] = x
         ys[i+1, :] = y
 
-    plt.plot(ts, xs[:, 0], label='x1')
-    plt.plot(ts, xs[:, 1], label='x2')
-    plt.plot(ts, us, label='u')
+    plt.plot(ts, pd, label='$\\theta_d$', color='k', linestyle='--')
+    plt.plot(ts, xs[:, 0], label='$\\theta$')
+    plt.plot(ts, xs[:, 1], label='$\dot{\\theta}$')
+    plt.plot(ts, us, label='$u$')
     plt.grid()
     plt.legend()
     plt.show()

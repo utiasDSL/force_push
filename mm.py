87be0dc0233284efd80ae432d76e6394930718e1
Adam Heins <mail@adamheins.com>
2019-11-25 14:17:51 -0500

    Start building 3DOF MM sim.

--- mm.py
+++ mm.py
@@ -7,119 +7,78 @@ import qpoases
 import IPython
 
 
-class Pendulum(object):
-    def __init__(self, dt):
-        self.dt = dt
-        self.g = 1.0
-        self.l = 1.0
+L1 = 1
+L2 = 2
 
-        self.m = 1  # state dimension
-        self.n = 2  # input dimension
-        self.p = 2  # output dimension
 
-    def motion(self, x, u):
-        ''' Motion model: x_k+1 = f(x_k, u_k) '''
-        # NOTE modulo math may make errors and state more sensical, but also
-        # discontinous
-        return x + self.dt * np.array([
-            x[1],
-            self.g * np.sin(x[0]) / self.l + u])
+class MM(object):
+    def __init__(self):
+        pass
 
-    def measure(self, x):
-        ''' Measurement model: y_k = g(u_k) '''
-        return x
+    def forward(self, q):
+        return np.array([q[0] + L1*np.cos(q[1]) + L2*np.cos(q[1]+q[2]),
+                                L1*np.sin(q[1]) + L2*np.sin(q[1]+q[2])])
 
-    def calc_A(self, x):
+    def jacobian(self, q):
         return np.array([
-            [1, self.dt],
-            [self.dt * self.g * np.cos(x[0]) / self.l, 1]])
-
-    def calc_B(self, x):
-        return np.array([[0], [1]])
-
-    def calc_C(self, x):
-        # return np.array([[1, 0]])
-        return np.eye(self.n)
+            [1, -L1*np.sin(q[1])-L2*np.sin(q[1]+q[2]), -L2*np.sin(q[1]+q[2])],
+            [0,  L1*np.cos(q[1])-L2*np.cos(q[1]+q[2]), -L2*np.cos(q[1]+q[2])]])
 
 
 class MPC(object):
     ''' Model predictive controller. '''
-    def __init__(self, sys, Q, R, lb, ub):
-        self.sys = sys
+    def __init__(self, model, dt, Q, R, lb, ub):
+        self.model = model
+        self.dt = dt
         self.Q = Q
         self.R = R
         self.lb = lb
         self.ub = ub
 
-    def _lookahead(self, x0, Yd, U, N):
+    def _lookahead(self, q0, pr, dq, N):
         ''' Generate lifted matrices proprogating the state N timesteps into the
             future.
 
             sys: System model.
-            x0:  Current state.
-            Yd:  Desired output trajectory.
-            U:   Input trajectory from the last iteration.
+            q0:  Current joint positions.
+            pr:  Desired Cartesian output trajectory.
+            dq:  Input joint velocties from the last iteration.
             Q:   Tracking error weighting matrix.
             R:   Input magnitude weighting matrix.
             N:   Number of timesteps into the future. '''
 
-        n = self.sys.n
-        m = self.sys.m
-        p = self.sys.p
+        n = 3  # number of joints
+        p = 2  # number of Cartesian outputs
 
-        # States from the last iteration
-        X_last = np.zeros(n*(N+1))
-        X_last[:n] = x0
+        qbar = np.zeros(n*(N+1))
+        qbar[:n] = q0
 
+        # Integrate joint positions from the last iteration
         for k in range(1, N+1):
-            x_prev = X_last[(k-1)*n:k*n]
-            u_prev = U[(k-1)*m:k*m]
-            x = self.sys.motion(x_prev, u_prev)
-            X_last[k*n:(k+1)*n] = x
+            q_prev = qbar[(k-1)*n:k*n]
+            dq_prev = dq[(k-1)*n:k*n]
+            q = q_prev + self.dt * dq_prev
 
-        # Arrays of the linearized matrices for each timestep.
-        As = np.zeros((n*(N+1), n))
-        Bs = np.zeros((n*N, m))
-        Cs = np.zeros((p*(N+1), n))
+            qbar[k*n:(k+1)*n] = q
 
-        # Calculate B matrices: B_0..B_{N-1}
-        for k in range(N):
-            x = X_last[k*n:(k+1)*n]
-            Bs[k*n:(k+1)*n, :] = self.sys.calc_B(x)
-
-        # Calculate A and C matrices: A_1..A_N, C_1..C_N
-        for k in range(1, N+1):
-            x = X_last[k*n:(k+1)*n]
-            As[k*n:(k+1)*n, :] = self.sys.calc_A(x)
-            Cs[k*p:(k+1)*p, :] = self.sys.calc_C(x)
-
-        Abar = np.zeros(p*N)
-        Bbar = np.zeros((p*N, m*N))
+        fbar = np.zeros(p*N)         # Lifted forward kinematics
+        Jbar = np.zeros((p*N, n*N))  # Lifted Jacobian
         Qbar = np.zeros((p*N, p*N))
-        Rbar = np.zeros((m*N, m*N))
+        Rbar = np.zeros((n*N, n*N))
 
-        # Build Abar matrix
         for k in range(N):
-            x = X_last[(k+1)*n:(k+2)*n]
-            Abar[k*p:(k+1)*p] = self.sys.measure(x)
+            q = qbar[(k+1)*n:(k+2)*n]
 
-        # Build Bbar matrix
-        for r in range(N):
-            for c in range(r+1):
-                B = Bs[c*n:(c+1)*n, :]
-                C = Cs[(r+1)*p:(r+2)*p, :]
-                A = np.eye(n)
-                for i in range(c+1, r+1):
-                    A = As[i*n:(i+1)*n, :].dot(A)
-                Bbar[r*p:(r+1)*p, c*m:(c+1)*m] = C.dot(A).dot(B)
+            fbar[k*np:(k+1)*np] = self.model.forward(q)
+            Jbar[k*np:(k+1)*np, k*n:(k+1)*n] = 0
 
-        # Build lifted weight matrices
-        for k in range(N):
             Qbar[k*p:(k+1)*p, k*p:(k+1)*p] = self.Q
-            Rbar[k*m:(k+1)*m, k*m:(k+1)*m] = self.R
+            Rbar[k*n:(k+1)*n, k*n:(k+1)*n] = self.R
 
-        H = Rbar + Bbar.T.dot(Qbar).dot(Bbar)
-        g = U.T.dot(Rbar) + (Abar - Yd).T.dot(Qbar).dot(Bbar)
+        dbar = fbar - pr
+
+        H = Rbar + self.dt**2*Jbar.T.dot(Qbar).dot(Jbar)
+        g = dq.T.dot(Rbar) + self.dt*dbar.T.dot(Qbar).dot(Jbar)
 
         return H, g
 

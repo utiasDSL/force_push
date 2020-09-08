c5fa15dd1a654bd0f47fe6918e0401052f5b8329
Adam Heins <mail@adamheins.com>
2020-09-08 15:36:10 -0400

    save before changing force sim structure

--- scripts/force_estimation.py
+++ scripts/force_estimation.py
@@ -7,9 +7,9 @@ import IPython
 
 
 # object
-ko = 100  # spring constant
+ko = 200  # spring constant
 
-fn = 100  # normal force
+fn = 50  # normal force
 mu = 1    # coefficient of friction
 wo = mu * fn
 
@@ -27,27 +27,58 @@ def main():
 
     u = vd  # input velocity for robot
 
-    for i in range(N - 1):
-        x[i+1] = x[i] + u * dt
-
-        if x[i+1] > xo[i]:
+    for i in range(N-1):
+        if x[i] > xo[i]:
             # contact
-            f[i+1] = ko * (x[i+1] - xo[i])
+            f_elastic = ko * (x[i] - xo[i])
 
-            # if elastic force is overcomes friction force, the object moves
+            # if elastic force overcomes friction force, the object moves
             # (which should result in lower elastic force next time)
-            if f[i+1] > wo:
+            if f_elastic > wo:
                 # these two formulae are equivalent:
                 # xo[i+1] = xo[i] + (f[i+1] - wo) / ko
-                xo[i+1] = x[i+1] - wo / ko
+                xo[i+1] = x[i] - wo / ko
             else:
                 xo[i+1] = xo[i]
         else:
-            f[i+1] = 0
+            f[i] = 0
+            xo[i+1] = xo[i]
+
+        x[i+1] = x[i] + u * dt
+
+
+    # NOTE we can find approximate k just by using:
+    #   A = x[500:] - xo[500:]
+    #   b = f[500:]
+    # but why can't I get w out of this, too?
+    # TODO maybe need to take velocity observations into account?
+
+    # ideally this would give back my ko and wo values
+    # A = np.vstack((x[500:] - xo[500:], np.ones(500))).T
+
+    # calculate object velocity
+    vo = np.zeros(N)
+    vo[1:] = (xo[1:] - xo[:-1]) / dt
+    moving = vo > 0
+
+    # we only care about points when the robot and object are actually in
+    # contact
+    contact = f > 0
+
+    A = np.vstack((x - xo, moving)).T
+    A = A[contact, :]
+    b = f[contact]
+
+    eps = 0
+    params = np.linalg.solve(A.T @ A + np.eye(2) * eps, A.T @ b)
+    print(params)
+
+    IPython.embed()
 
     plt.plot(t, x, label='x')
     plt.plot(t, f, label='f')
     plt.plot(t, xo, label='xo')
+    plt.plot(t, vo, label='vo')
     plt.grid()
     plt.legend()
     plt.show()

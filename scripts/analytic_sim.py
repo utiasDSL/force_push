8d5eaf2d0febea35c563ccfc66c99801b0705f28
Adam Heins <mail@adamheins.com>
2023-04-15 14:37:42 -0400

    Infrastructure for sets of simulations; some testing of circle.

--- scripts/analytic_sim.py
+++ scripts/analytic_sim.py
@@ -1,173 +1,20 @@
 """Simulation of pushing based on quasistatic model and my control law."""
 import numpy as np
-from scipy import sparse
-import osqp
 import matplotlib.pyplot as plt
+import time
 from mmpush import *
+
 import IPython
 
 
-class QuadSlider:
-    def __init__(self, hx, hy):
-        """Quadrilateral slider with half lengths hx and hy."""
-        self.hx = hx
-        self.hy = hy
-
-    def contact_point(self, s):
-        """Contact point given parameter s."""
-        # for a quad, s is the y position along the face
-        return np.array([-self.hx, s])
-
-    def contact_normal(self, s):
-        """Inward-facing contact normal."""
-        # constant for a quad
-        return np.array([1, 0])
-
-    def s_dot(self, α):
-        """Time derivative of s."""
-        return α
-
-
-class CircleSlider:
-    def __init__(self, r):
-        """Circle slider with radius r."""
-        self.r = r
-
-    def contact_point(self, s):
-        # for a quad, s is the angle of the contact point
-        return rot2d(s) @ [-self.r, 0]
-
-    def contact_normal(self, s):
-        return np.array([np.cos(s), np.sin(s)])
-
-    def s_dot(self, α):
-        return α / self.r
-
-
-class QPMotion:
-    """Quadratic program-based equations of motion."""
-
-    def __init__(self, M, μ):
-        self.M = M
-        self.μ = μ
-
-        # cost is constant
-        P = np.diag([1.0, 0, 0])
-
-        # establish constraint sparsity pattern
-        A, self.A_idx = self._A_sparsity_pattern()
-
-        # initial problem setup
-        self.problem = osqp.OSQP()
-        self.problem.setup(
-            P=sparse.csc_matrix(P),
-            l=np.zeros(5),
-            u=np.zeros(5),
-            A=sparse.csc_matrix(A),
-            verbose=False,
-            eps_abs=1e-6,
-            eps_rel=1e-6,
-        )
-
-    @staticmethod
-    def _A_sparsity_pattern():
-        """Compute the sparsity pattern of A."""
-        A = np.ones((5, 3))
-        A[2:, 0] = 0
-        # indices are taken w.r.t. column-wise flattened data
-        return A, np.nonzero(A.T.flatten())
-
-    def _A_data(self, A):
-        """Get data from A as required by CSC sparse matrix format."""
-        return A.T.flatten()[self.A_idx]
-
-    def _compute_constraints(self, vp, W, nc, nc_perp):
-        """Compute updated constraint matrix and vectors l <= Ax <= u"""
-        # motion cone constraint
-        Lv = vp
-        Uv = vp
-        Av = np.hstack((nc_perp[:, None], W.T @ self.M @ W))
-
-        # friction cone constraint
-        Lf = -np.inf * np.ones(3)
-        Uf = np.zeros(3)
-        # fmt: off
-        Af = np.hstack((np.zeros((3, 1)), np.vstack((
-            -nc,
-            -nc_perp - self.μ * nc,
-            nc_perp - self.μ * nc,
-        ))))
-        # fmt: on
-
-        # put them together
-        L = np.concatenate((Lv, Lf))
-        U = np.concatenate((Uv, Uf))
-        A = np.vstack((Av, Af))
-
-        return A, L, U
-
-    def solve(self, vp, W, nc):
-        """Update and solve the problem with the new data."""
-        nc_perp = perp2d(nc)
-        A, L, U = self._compute_constraints(vp, W, nc, nc_perp)
-
-        self.problem.update(Ax=self._A_data(A), l=L, u=U)
-        res = self.problem.solve()
-
-        α = res.x[0]
-        f = res.x[1:]
-        v_slip = α * nc_perp
-
-        # recover object velocity
-        vo = vp - v_slip
-        Vo = self.M @ W @ f
-
-        return Vo, f, α
-
-
-class CirclePath:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def _closest_point(self, p):
-        return self.radius * unit(p)
-
-    def compute_normal(self, p):
-        # TODO this is stupid
-        # s = np.arctan2(p[1], p[0])
-        # dxds = np.cos(s)
-        # dyds = np.sin(s)
-        # return np.array([dxds, dyds])
-        return unit(p)
-
-    def compute_lateral_offset(self, p):
-        c = self._closest_point(p)
-        n = self.compute_normal(p)
-        return n @ (p - c)
-
-    def compute_travel_direction(self, p):
-        return rot2d(np.pi/2) @ self.compute_normal(p)
-
-
-class StraightPath:
-    def __init__(self, direction, origin=None):
-        if origin is None:
-            origin = np.zeros(2)
-        self.origin = origin
-        self.direction = direction
-        self.perp = rot2d(np.pi/2) @ direction
-
-    def compute_travel_direction(self, p):
-        return self.direction
-
-    def compute_lateral_offset(self, p):
-        return self.perp @ (p - self.origin)
-
-
-def simulate_pushing(motion, slider, path, kθ, ky, x0, duration, timestep):
+def simulate_pushing(motion, slider, path, speed, kθ, ky, x0, duration, timestep):
+    """Simulate pushing a slider with single-point contact."""
     x = x0.copy()
     xs = [x0.copy()]
+    us = []
     ts = [0]
+
+    success = True
 
     t = 0
     while t < duration:
@@ -177,76 +24,271 @@ def simulate_pushing(motion, slider, path, kθ, ky, x0, duration, timestep):
         C_wb = rot2d(φ)
         f = C_wb.T @ x[4:]
         nc = slider.contact_normal(s)
-        r_co_o = slider.contact_point(s)
+        # TODO catch error for sliding off
+        try:
+            r_co_o = slider.contact_point(s) - slider.cof
+        except ValueError:
+            print(f"Contact point left the slider at time = {t}!")
+            success = False
+            break
         W = np.array([[1, 0], [0, 1], [-r_co_o[1], r_co_o[0]]])
 
         # term to correct deviations from desired line
         # this is simpler than pure pursuit!
         r_cw_w = x[:2] + C_wb @ r_co_o
-        # θy = ky * r_cw_w[1]
         Δ = path.compute_travel_direction(r_cw_w)
         θy = ky * path.compute_lateral_offset(r_cw_w)
+        # print(path.compute_lateral_offset(r_cw_w))
 
         # angle-based control law
         θd = signed_angle(Δ, C_wb @ unit(f))
-        θ = (1 + kθ) * θd + θy
-        vp = 1 * C_wb.T @ rot2d(θ) @ Δ
+        θv = (1 + kθ) * θd + θy
+        vp = speed * C_wb.T @ rot2d(θv) @ Δ
 
         # equations of motion
-        Vo, f, α = motion.solve(vp, W, nc)
+        try:
+            Vo, f, α = motion.solve(vp, W, nc)
+        except TypeError:
+            print(f"Solver failed at time = {t}!")
+            success = False
+            break
 
         # update state
         x[:2] += timestep * C_wb @ Vo[:2]
-        x[2] += timestep * Vo[2]
+        x[2] = wrap_to_pi(x[2] + timestep * Vo[2])
         x[3] += timestep * slider.s_dot(α)
         x[4:] = C_wb @ f
 
         t += timestep
 
+        us.append(vp)
         xs.append(x.copy())
         ts.append(t)
 
-    return np.array(ts), np.array(xs)
+    return success, np.array(ts), np.array(xs), np.array(us)
+
+
+def make_line(a, b, color="k"):
+    return plt.Line2D([a[0], b[0]], [a[1], b[1]], color=color, linewidth=1)
+
+
+def update_line(line, a, b):
+    line.set_xdata([a[0], b[0]])
+    line.set_ydata([a[1], b[1]])
+
+
+def playback_simulation(xs, us, slider, path, sleep, step=1):
+    plt.ion()
+    fig = plt.figure()
+    ax = plt.gca()
+
+    ax.set_xlabel("x (m)")
+    ax.set_ylabel("y (m)")
+    ax.set_xlim([-5, 15])
+    ax.set_ylim([-10, 10])
+    ax.grid()
+
+    ax.set_aspect("equal")
+
+    x = xs[0, :]
+    φ = x[2]
+    s = x[3]
+    C_wb = rot2d(φ)
+    f = x[4:]  # world frame
+    r_co_o = slider.contact_point(s)
+    r_cw_w = x[:2] + C_wb @ r_co_o
+    vp = C_wb @ us[0, :]
+
+    if type(slider) is CircleSlider:
+        patch = plt.Circle(
+            xs[0, :2],
+            radius=slider.r,
+        )
+    elif type(slider) is QuadSlider:
+        patch = plt.Rectangle(
+            xs[0, :2] - [slider.hx, slider.hy],
+            width=2 * slider.hx,
+            height=2 * slider.hy,
+            angle=np.rad2deg(φ),
+            rotation_point="center",
+        )
+    ax.add_patch(patch)
+
+    push_line = make_line(r_cw_w, r_cw_w + unit(vp), color="r")
+    ax.add_line(push_line)
+
+    force_line = make_line(r_cw_w, r_cw_w + unit(f), color="b")
+    ax.add_line(force_line)
+
+    c = path.compute_closest_point(r_cw_w)
+    deviation_line = make_line(r_cw_w, c, color="g")
+    ax.add_line(deviation_line)
+
+    Δ = path.compute_travel_direction(r_cw_w)
+    des_line = make_line(r_cw_w, r_cw_w + Δ, color="k")
+    ax.add_line(des_line)
+
+    for i in range(1, len(us), step):
+        x = xs[i, :]
+
+        φ = x[2]
+        s = x[3]
+        C_wb = rot2d(φ)
+        f = x[4:]  # world frame
+        r_co_o = slider.contact_point(s)
+        r_cw_w = x[:2] + C_wb @ r_co_o
+        vp = C_wb @ us[i, :]
+
+        Δ = path.compute_travel_direction(r_cw_w)
+        c = path.compute_closest_point(r_cw_w)
+
+        if type(slider) is CircleSlider:
+            patch.center = x[:2]
+        elif type(slider) is QuadSlider:
+            patch.xy = x[:2] - [slider.hx, slider.hy]
+            patch.angle = np.rad2deg(φ)
+        update_line(push_line, r_cw_w, r_cw_w + unit(vp))
+        update_line(force_line, r_cw_w, r_cw_w + unit(f))
+        update_line(des_line, r_cw_w, r_cw_w + Δ)
+        update_line(deviation_line, r_cw_w, c)
+
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        time.sleep(sleep)
+    plt.ioff()
+
+
+def simulate_many(
+    motion, slider, path, speed, kθ, ky, duration, timestep, y0s, θ0s, sθs, μs
+):
+    """Simulate many pushes, one for each value of x0."""
+    x0s = []
+
+    all_ts = []
+    all_xs = []
+    successes = []
+    all_μs = []
+
+    for μ in μs:
+        motion.μ = μ
+        for y0 in y0s:
+            for θ0 in θ0s:
+                for s0 in sθs:
+                    x0 = np.array([0., y0, θ0, s0, 1, 0])
+                    x0s.append(x0)
+                    success, ts, xs, us = simulate_pushing(
+                        motion, slider, path, speed, kθ, ky, x0, duration, timestep
+                    )
+                    successes.append(success)
+                    all_ts.append(ts)
+                    all_xs.append(xs)
+                    all_μs.append(μ)
+    return successes, all_ts, all_xs, all_μs
 
 
 def main():
-    # path = CirclePath(1.0)
-    # IPython.embed()
-    # return
-
     direction = np.array([1, 0])
     path = StraightPath(direction)
+    # path = CirclePath(1.0)
+
+    speed = 0.5
 
     f_max = 5
     τ_max = 0.1
     M = np.diag([1.0 / f_max**2, 1 / f_max**2, 1.0 / τ_max**2])
-    μ = 0.2
+    μ = 0
 
     # control gains
-    kθ = 0.2
-    ky = 0.2
+    kθ = 1.0
+    ky = 1.0
 
-    x0 = np.array([0.0, 0.1, 0, 0, 1, 0])
+    # x = (x, y, θ, s, f_x, f_y)
+    x0 = np.array([0.0, -0.2, 0, 0, 1, 0])
 
     motion = QPMotion(M, μ)
-    slider = QuadSlider(0.5, 0.5)
-    # slider = CircleSlider(0.5)
+    # slider = QuadSlider(0.5, 0.5, cof=cof)
 
-    duration = 100
-    timestep = 0.1
+    duration = 10
+    timestep = 0.01
 
-    ts, xs = simulate_pushing(motion, slider, path, kθ, ky, x0, duration, timestep)
+    slider = CircleSlider(0.5)
+    successes, ts, xs, us = simulate_pushing(
+        motion, slider, path, speed, kθ, ky, x0, duration, timestep
+    )
+    playback_simulation(xs, us, slider, path, sleep=0.001)
+    return
+
+    y0s = [-0.2, 0, 0.2]
+    θ0s = [-0.2, 0, 0.2]
+    s0s = [-0.2, 0, 0.2]
+    μs = [0, 0.25, 0.5]
+
+    slider = QuadSlider(0.5, 0.5, cof=[0., 0.])
+    successes, ts, xs, μs1 = simulate_many(motion, slider, path, speed, kθ, ky, duration, timestep, y0s, θ0s, s0s, μs)
+    n = len(ts)
 
     plt.figure()
-    plt.plot(ts, xs[:, 0], label="x")
-    plt.plot(ts, xs[:, 1], label="y")
-    plt.plot(ts, xs[:, 2], label="φ")
-    plt.plot(ts, xs[:, 3], label="s")
-    plt.plot(ts, xs[:, 4], label="fx")
-    plt.plot(ts, xs[:, 5], label="fy")
-    plt.legend()
-    plt.grid()
+    for i in range(n):
+        plt.plot(xs[i][:, 0], xs[i][:, 1], color="b", alpha=0.1)
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    plt.title("Square slider")
+
+    slider = QuadSlider(0.5, 0.5, cof=[-0.1, -0.1])
+    successes, ts, xs, μs2 = simulate_many(motion, slider, path, speed, kθ, ky, duration, timestep, y0s, θ0s, s0s, μs)
+    n = len(ts)
+
+    plt.figure()
+    for i in range(n):
+        plt.plot(xs[i][:, 0], xs[i][:, 1], color="r", alpha=0.1)
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    plt.title("Square slider, cof offset")
+
+    slider = CircleSlider(0.5)
+    successes, ts, xs, μs3 = simulate_many(motion, slider, path, speed, kθ, ky, duration, timestep, y0s, θ0s, s0s, μs)
+    n = len(ts)
+
+    plt.figure()
+    for i in range(n):
+        plt.plot(xs[i][:, 0], xs[i][:, 1], color="g", alpha=0.1)
+        if not successes[i]:
+            print(f"circle failed with x0 = {xs[i][0, :]}, μ = {μs3[i]}")
+    plt.xlabel("x [m]")
+    plt.ylabel("y [m]")
+    plt.title("Circle slider")
+
     plt.show()
+
+    # n = 11
+    # y_values = np.linspace(-1.5, 1.5, n)
+    # s_values = np.linspace(-0.4, 0.4, n)
+    # convergence = np.zeros((n, n))
+    # for i, y in enumerate(y_values):
+    #     for j, s in enumerate(s_values):
+    #         x0 = np.array([0, y, 0, s, 1, 0])
+    #         ts, xs = simulate_pushing(
+    #             motion, slider, path, speed, kθ, ky, x0, duration, timestep
+    #         )
+    #         print(f"s_max = {np.max(xs[:, 3])}")
+    #         if np.max(xs[:, 3]) > 0.5:
+    #             continue
+    #         y_final = xs[-1, 1]
+    #         θf_final = np.arctan2(xs[-1, 5], xs[-1, 4])
+    #         print(f"y = {y_final}")
+    #         print(f"θf = {θf_final}")
+    #         # IPython.embed()
+    #         # return
+    #         if np.abs(y_final) < 1e-3 and np.abs(θf_final) < 1e-3:
+    #             convergence[i, j] = 1
+    #
+    # # TODO can we do this as a non-3D grid?
+    # x, y = np.meshgrid(y_values, s_values)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection="3d")
+    # ax.scatter(x, y, convergence)
+    # plt.grid()
+    # plt.show()
 
 
 if __name__ == "__main__":
